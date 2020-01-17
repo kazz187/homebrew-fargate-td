e3b82ff91dd691960150f24ccec1503283850d36
Kazuki Akamine <kazuki.akamine@showcase-gig.com>
2020-01-17 15:42:06 +0900

    Update script

--- update.sh
+++ update.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sed "s/<VERSION>/$1/g" template/fargate-td.rb | sed "s/<SHA256>/$2/g" > Formula/fargate-td.rb
+TAG=$1
+SHA256=$2
+
+sed "s/<VERSION>/${TAG/v/}/g" template/fargate-td.rb | sed "s/<SHA256>/${SHA256}/g" > Formula/fargate-td.rb
