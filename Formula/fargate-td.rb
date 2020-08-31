47cb1404ddabf5708099b9db9223012d0272dcad
kazzbot <kazzone87@gmail.com>
2020-08-31 17:50:31 +0000

    Update fargate-td v0.0.24

--- Formula/fargate-td.rb
+++ Formula/fargate-td.rb
@@ -1,9 +1,9 @@
 class FargateTd < Formula
   desc "Generator of amazon fargate task definitions"
   homepage "https://github.com/kazz187/fargate-td"
-  url "https://github.com/kazz187/fargate-td/releases/download/v0.0.23/fargate-td_darwin_amd64.zip"
-  version "0.0.23"
-  sha256 "e1b408c1679d4df51197422a0e906cbe3c64c7557863d1641afe7b3bc55453d2"
+  url "https://github.com/kazz187/fargate-td/releases/download/v0.0.24/fargate-td_darwin_amd64.zip"
+  version "0.0.24"
+  sha256 "07786aca662fbd21e4dcb4525ebf63dc610eb0818adaa1761aaf96a1c58c0342"
 
   def install
     bin.install "fargate-td"
