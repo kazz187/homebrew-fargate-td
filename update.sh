76c339c1a84c1add5691acf7b569a0bfa41828fa
Kazuki Akamine <kazuki.akamine@showcase-gig.com>
2022-07-06 19:41:25 +0900

    Support M1 mac

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@
 
 TAG=$1
 SHA256=$2
+SHA256ARM=$3
 
 sed "s/<VERSION>/${TAG/v/}/g" template/fargate-td.rb | sed "s/<SHA256>/${SHA256}/g" > Formula/fargate-td.rb
+sed "s/<VERSION>/${TAG/v/}/g" template/fargate-td.rb | sed "s/<SHA256>/${SHA256ARM}/g" | sed "s/FargateTd/FargateTdArm/g" > Formula/fargate-td-arm.rb
