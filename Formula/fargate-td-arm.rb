fffab0219fe707a1b1156dc19b57640a59ff63ce
kazzbot <kazzone87@gmail.com>
2022-07-06 11:51:51 +0000

    Update fargate-td v0.0.27

--- Formula/fargate-td-arm.rb
+++ Formula/fargate-td-arm.rb
@@ -1,9 +1,9 @@
 class FargateTdArm < Formula
   desc "Generator of amazon fargate task definitions"
   homepage "https://github.com/kazz187/fargate-td"
-  url "https://github.com/kazz187/fargate-td/releases/download/v0.0.26/fargate-td_darwin_amd64.zip"
-  version "0.0.26"
-  sha256 "d49f15f53f3a83db3bed6fe1a6730682e21442f1929009a8bd3ea0b8bc59e675"
+  url "https://github.com/kazz187/fargate-td/releases/download/v0.0.27/fargate-td_darwin_arm64.zip"
+  version "0.0.27"
+  sha256 "e67efc38d5b9025a9bbe1e42338b255a318dd2f3cd83f06ab558f6252580d8bc"
 
   def install
     bin.install "fargate-td"
--- Formula/fargate-td.rb
+++ Formula/fargate-td.rb
@@ -1,9 +1,9 @@
 class FargateTd < Formula
   desc "Generator of amazon fargate task definitions"
   homepage "https://github.com/kazz187/fargate-td"
-  url "https://github.com/kazz187/fargate-td/releases/download/v0.0.26/fargate-td_darwin_amd64.zip"
-  version "0.0.26"
-  sha256 "a692494893af27dfa06d1b21a2165c56bcaa5b1e90e43238df004d5e0fcb7191"
+  url "https://github.com/kazz187/fargate-td/releases/download/v0.0.27/fargate-td_darwin_amd64.zip"
+  version "0.0.27"
+  sha256 "dcd6c262e715bf4845b4cafa7921249675e2639b4833e8c6cdfe50846b0f81c8"
 
   def install
     bin.install "fargate-td"
